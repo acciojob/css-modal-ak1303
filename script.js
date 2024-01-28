e62f48304cd1479e588bd6ee703f44d098ca957d
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-01-28 19:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-//your JS code here. If required.
-var openBtn = document.querySelector('.open');
-var model = document.querySelector('.model');
-openBtn.addEventListener("click", function () {
-  model.style.display = "block";
-  model.classList.add("loaded");
-  document.body.style.backgroundColor = "lightgrey";
-});
-var closeBtn = document.querySelector('.close-modal');
-closeBtn.addEventListener("click", function () {
-  model.style.display = "none";
-  document.body.style.backgroundColor = "white";
-});
+ var modal = document.querySelector('.model');
+    var openBtn = document.querySelector('.open');
 
+    // Function to open the modal
+    function openModal() {
+        modal.style.display = 'block';
+        document.body.style.backgroundColor = 'lightgrey';
+		modal.style.top = '20%';
+	    modal.style.transform = 'translateY(-50%)';
+		setTimeout(function() {
+	        modal.style.transition = 'none';
+	    }, 500);
+    }
 
+    // Function to close the modal
+    function closeModal() {
+		modal.style.top = '-100%';
+	    modal.style.transform = 'translateY(0)';
+        document.body.style.backgroundColor = 'white';
+		modal.style.transition="top 0.5s, transform 0.5s";
+    }
+
+    // Close the modal if the user clicks outside of it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    };
