4373a76ebb7f69856a83724b20c0923accc0f8dc
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-01-28 18:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var openBtn = document.querySelector('.open');
+var model = document.querySelector('.model');
+openBtn.addEventListener("click", function () {
+  model.style.display = "block";
+  model.classList.add("loaded");
+  document.body.style.backgroundColor = "lightgrey";
+});
+var closeBtn = document.querySelector('.close-modal');
+closeBtn.addEventListener("click", function () {
+  model.style.display = "none";
+  document.body.style.backgroundColor = "white";
+});
+
+
