9f191f03453fffa3e3e2f6e5af7b1b42155e8c34
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-01-28 22:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var modal = document.querySelector('.modal');
+var btn = document.getElementById('openModal');
 
-	function openModal() {
-	    modal.classList.add('open');
-	    document.body.style.backgroundColor = 'lightgrey';
-	}
+function openModal() {
+    modal.classList.add('open');
+    document.body.style.backgroundColor = 'lightgrey';
+    window.addEventListener("click", closeIfClickedOutside);
+}
 
-    function closeModal() {
-        document.body.style.backgroundColor = 'white';
-		modal.classList.remove('open');
+function closeModal() {
+    document.body.style.backgroundColor = 'white';
+    modal.classList.remove('open');
+    window.removeEventListener("click", closeIfClickedOutside);
+}
+
+function closeIfClickedOutside(event) {
+    if (!modal.contains(event.target) && event.target !== btn) {
+        closeModal();
     }
+}
