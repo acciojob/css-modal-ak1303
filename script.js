10973be55e6eb19bfec8ea64ad1ff2aa5a867c19
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-01-28 21:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
- var modal = document.querySelector('.model');
-    var openBtn = document.querySelector('.open');
+var modal = document.querySelector('.modal');
 
-    // Function to open the modal
-    function openModal() {
-        modal.style.display = 'block';
-        document.body.style.backgroundColor = 'lightgrey';
-		modal.style.top = '20%';
-	    modal.style.transform = 'translateY(-50%)';
-		setTimeout(function() {
-	        modal.style.transition = 'none';
-	    }, 500);
-    }
+	function openModal() {
+	    modal.classList.add('open');
+	    document.body.style.backgroundColor = 'lightgrey';
+	}
 
-    // Function to close the modal
     function closeModal() {
-		modal.style.top = '-100%';
-	    modal.style.transform = 'translateY(0)';
+		modal.classList.remove('open');
         document.body.style.backgroundColor = 'white';
-		modal.style.transition="top 0.5s, transform 0.5s";
     }
-
-    // Close the modal if the user clicks outside of it
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            closeModal();
-        }
-    };
